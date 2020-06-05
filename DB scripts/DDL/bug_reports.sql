3979a12230008a6b7faa65327f4f870aa551686c
Bryan Friestad <btrf521@gmail.com>
2020-06-04 21:12:19 -0500

    Create bug_reports.sql

---
+++ DB scripts/DDL/bug_reports.sql
@@ -0,0 +1,13 @@
+use db309sd1;
+#drop table if exists bug_reports;
+create table bug_reports(
+	id int NOT NULL auto_increment,
+	reporting_user varchar(255) NOT NULL,
+    report_date date NOT NULL,
+    short_description varchar(50) NOT NULL,
+    location varchar(30) NOT NULL,
+    encounter_date date NOT NULL,
+    long_description varchar(500),
+    primary key (id),
+    foreign key (sender) references account (UserID) on delete no action on update cascade
+);
